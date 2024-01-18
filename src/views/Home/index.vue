2f5d0c75b5018551663759f38da6dc52c2d52c9b
José Victor Pereira Lima <josevictorp81@gmail.com>
2024-01-18 16:57:37 -0300

    overriding tailwind configs and add font family

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,7 +6,7 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="text-3xl text-justify w-1/2">
+  <div class="container">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ipsum unde commodi eaque totam dolores maxime? Maxime temporibus minima rem quas, labore fugiat! Facere animi cumque, qui quasi ullam quibusdam.
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'weather-primary': '#00668a',
+        'weather-secondary': '#004E71',
+      },
+    },
+    fontFamily: {
+      roboto: ['Roboto, sans-serif'],
+    },
+    container: {
+      padding: '2rem',
+      center: true,
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+    },
   },
   plugins: [],
 }
