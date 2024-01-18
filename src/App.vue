4db9e6d9976f75156d19efdcc665832d63b6853f
José Victor Pereira Lima <josevictorp81@gmail.com>
2024-01-18 17:39:00 -0300

    navigation bar component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import Navigation from './components/Navigation/index.vue'
 
 </script>
 
 <template>
-  <RouterView />
+  <div class="flex flex-col min-h-screen font-roboto bg-weather-primary">
+    <Navigation />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
