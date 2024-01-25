01a2382642694fb73d76ebb48c19900860f357c0
José Victor Pereira Lima <josevictorp81@gmail.com>
2024-01-24 21:09:41 -0300

    view city

--- src/views/City/index.vue
+++ src/views/City/index.vue
@@ -1,11 +1,21 @@
 <script lang='ts'>
 import { defineComponent } from 'vue'
+import AsyncCity from '../../components/AsyncCity/index.vue'
 
 export default defineComponent({
-
+  components: { AsyncCity }
 })
 </script>
 
-<template></template>
+<template>
+  <div>
+    <Suspense>
+      <AsyncCity />
+      <template #fallback>
+        A inserir....
+      </template>
+    </Suspense>
+  </div>
+</template>
 
 <style scoped></style>
