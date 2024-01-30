0d7932afba20109334a19092ee5940b6661f959d
José Victor Pereira Lima <josevictorp81@gmail.com>
2024-01-29 21:55:52 -0300

    city list component

---
+++ src/components/CityList/index.vue
@@ -0,0 +1,59 @@
+<script lang='ts'>
+import { defineComponent, reactive } from 'vue'
+import { useRouter } from 'vue-router'
+import services from '../../services'
+import { City, ObjectLocal } from '../../types/cityResult'
+import CityCard from '../CityCard/index.vue'
+
+interface SetupReturn {
+  state: City
+  showCityView: (city: ObjectLocal) => void
+
+}
+
+export default defineComponent({
+  components: { CityCard },
+  async setup(): Promise<SetupReturn> {
+    const state = reactive<City>({
+      cities: []
+    })
+
+    const router = useRouter()
+
+    async function getCities() {
+      if (localStorage.getItem('savedCities')) {
+        const saved: City = JSON.parse(localStorage.getItem('savedCities') as string)
+        state.cities = saved.cities
+      }
+
+      state.cities.forEach(async (city, index) => {
+        const { data } = await services.weather.getWeatherData(city.coords.lat, city.coords.lon)
+        state.cities[index].weather = data
+      })
+    }
+
+    await getCities()
+
+    function showCityView(city: ObjectLocal) {
+      router.push({ name: 'city', params: { state: city.state, city: city.city }, query: { lat: city.coords.lat, lon: city.coords.lon } })
+    }
+
+    return {
+      state,
+      showCityView,
+    }
+  }
+})
+</script>
+
+<template>
+  <div v-for="city in state.cities" :key="city.id">
+    <CityCard :city="city" @click="showCityView(city)" />
+  </div>
+
+  <p v-if="!state.cities.length">
+    Sem cidades salvas. Começe a observar um local buscando no campo acima.
+  </p>
+</template>
+
+<style scoped></style>../../types/cityResult
