5ba7fd3c06ac1f2c6a56a04b0d1b7b1fbe6c2f50
José Victor Pereira Lima <josevictorp81@gmail.com>
2024-01-30 15:49:34 -0300

    dynamic title

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  NavigationGuardNext,
+  RouteLocationNormalized,
+  createRouter,
+  createWebHistory,
+} from 'vue-router'
 
 type Route = {
   name: string
   path: string
   component: object
+  meta: { title: string }
 }
 
 const Home = () => import('../views/Home/index.vue')
@@ -14,14 +20,33 @@ const routes: Route[] = [
     path: '/',
     name: 'home',
     component: Home,
+    meta: {
+      title: 'Home',
+    },
   },
   {
     path: '/weather/:state/:city',
     name: 'city',
     component: City,
+    meta: {
+      title: 'Weather',
+    },
   },
 ]
 
 const router = createRouter({ history: createWebHistory('/'), routes })
+
+router.beforeEach(
+  (
+    to: RouteLocationNormalized,
+    _: RouteLocationNormalized,
+    next: NavigationGuardNext
+  ) => {
+    document.title = `${
+      to.params.state ? `${to.params.city}, ${to.params.state}` : to.meta.title
+    } | The Local Weather`
+    next()
+  }
+)
 
 export default router
